6e7eeae316feb0d875aeffdb953e22469684935f
Matthias Mayr <matthias.mayr@unibw.de>
2025-07-23 15:26:12 +0200

    Update meshing script for cubit

---
+++ compute_mesh_sizes.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+print("ID\tn_nodes\tn_dofs\tn_procs")
+
+for n_nodes_short in range(1,30):
+    n_nodes = 5 * (n_nodes_short ** 3)
+    n_dofs = n_nodes * 3
+    n_procs = n_dofs / 30000
+
+    print(f"{n_nodes_short}\t{n_nodes}\t{n_nodes * 3}\t{n_procs}")
