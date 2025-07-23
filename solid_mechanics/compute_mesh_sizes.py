cd6c1b342c8cd0dfba56333bd4543dd081f1b8bb
Matthias Mayr <matthias.mayr@unibw.de>
2025-07-23 15:36:42 +0200

    Update mesh files accroding to weak scaling schedule
    Provide different meshes with roughly 30k unknowns per MPI rank

--- solid_mechanics/compute_mesh_sizes.py
+++ solid_mechanics/compute_mesh_sizes.py
@@ -1,10 +1,10 @@
 #!/bin/python3
 
-print("ID\tn_nodes\tn_dofs\tn_procs")
+print("ID\tn_x\tn_y\tn_z\tn_nodes\tn_dofs\tn_procs")
 
 for n_nodes_short in range(1,30):
     n_nodes = 5 * (n_nodes_short ** 3)
     n_dofs = n_nodes * 3
     n_procs = n_dofs / 30000
 
-    print(f"{n_nodes_short}\t{n_nodes}\t{n_nodes * 3}\t{n_procs}")
+    print(f"{n_nodes_short}\t{n_nodes_short}\t{n_nodes_short}\t{n_nodes_short * 5}\t{n_nodes}\t{n_nodes * 3}\t{n_procs}")
